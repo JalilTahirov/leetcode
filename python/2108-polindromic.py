9910eed1bbb5301abb67db801300b212982beb2f
Jalil Tahirov <jalil.tahirov@outlook.com>
2024-07-05 06:58:58 +0400

    find polindromic

---
+++ python/2108-polindromic.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for w in words:
+            if self.is_polindrome(w):
+                return w
+        return ""
+    def is_polindrome(self, word:str):
+        return word == ''.join(reversed(word))
