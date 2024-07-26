2ebc22482478c4f06c37757f291b0e56968d4b1e
Jalil Tahirov <jalil.tahirov@outlook.com>
2024-07-26 06:29:33 +0400

    BST 700

--- dsa samples/bst_search.py
+++ dsa samples/bst_search.py
@@ -1,10 +1,27 @@
 class TreeNode:
-    def __init__(val):
+    def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
 
 root = TreeNode(2)
 root.left = TreeNode(1)
-root.right = TreeNode(3)
-root.right.right = TreeNode(4)
+root.right = TreeNode(4)
+root.right.left = TreeNode(3)
+root.right.right = TreeNode(5)
+
+def search(curr: TreeNode, number: int):
+    if not curr:
+        return False
+    if curr.val > number:
+        return search(curr.left, number)
+    elif curr.val < number:
+        return search(curr.right, number)
+    else:
+        return True
+
+res = search(root, 5)
+print(res)
+
+
+
